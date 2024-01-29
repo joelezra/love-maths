09d1d805e11a740b7cdceced61a1cdf0aa761d7d
Joel Joe <joelezra10@gmail.com>
2024-01-29 23:37:38 +0000

    Added division game function

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,7 +47,9 @@ function runGame (gameType) {
         displaySubtractQuestion(num1,num2);
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1,num2);
-    } else {
+    } else if (gameType === "division") {
+		displayDivisionQuestion(num1, num2);
+	} else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
@@ -91,7 +93,9 @@ function calculateCorrectAnswer() {
         return [operand1 - operand2, "subtract"];
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
-    } else {
+    } else if (operator ==="/") {
+		return [operand1 / operand2, "division"];
+	} else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -131,4 +135,12 @@ function displayMultiplyQuestion(operand1,operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+
+	document.getElementById("operand1").textContent = operand1*operand2;
+	document.getElementById("operand2").textContent = operand2;
+	document.getElementById("operator").textContent = "/";
+
 }
