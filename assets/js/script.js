244e86a19d799e8bf76e10c71f8904a802ee3b80
Joel Joe <joelezra10@gmail.com>
2024-01-29 18:22:25 +0000

    removed content from displaySubtractQuestion

--- assets/js/script.js
+++ assets/js/script.js
@@ -108,9 +108,7 @@ function displayAdditionQuestion(operand1,operand2) {
 }
 
 function displaySubtractQuestion(operand1,operand2) {
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = "-";
+    
 }
 
 function displayMultiplyQuestion(operand1,operand2) {
